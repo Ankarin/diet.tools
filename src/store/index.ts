6635278e7f813a8444ac00a335b291e8a4e455e7
mitry <ankarn41k@gmail.com>
2024-10-19 18:38:44 +0300

    supa

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+import supabase from "@/utils/supabase/client";
 import { create } from "zustand";
 
 type FormData = {
@@ -11,7 +12,6 @@ type FormData = {
   waist: string;
   hip: string;
   neck: string;
-
   activityLevel: string;
   dietaryRestrictions: string[];
   allergies: string;
@@ -23,11 +23,12 @@ type FormData = {
 type FormStore = {
   currentStep: number;
   formData: FormData;
+  goNextStep: () => void;
   setCurrentStep: (step: number) => void;
   updateFormData: (field: keyof FormData, value: string | string[]) => void;
 };
 
-export const useFormStore = create<FormStore>((set) => ({
+export const useFormStore = create<FormStore>((set, get) => ({
   currentStep: 1,
   formData: {
     gender: "" as "male" | "female",
@@ -40,7 +41,6 @@ export const useFormStore = create<FormStore>((set) => ({
     waist: "",
     hip: "",
     neck: "",
-
     activityLevel: "",
     dietaryRestrictions: [],
     allergies: "",
@@ -48,9 +48,94 @@ export const useFormStore = create<FormStore>((set) => ({
     mealPreferences: "",
     cookingSkill: "1",
   },
-  setCurrentStep: (step) => set({ currentStep: step }),
-  updateFormData: (field, value) =>
+  goNextStep: async () => {
+    const { currentStep, formData } = get();
+    const newStep = currentStep + 1;
+
+    // Get the current user
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    // If no user, sign in anonymously
+    if (!user) {
+      const { error } = await supabase.auth.signInAnonymously();
+      if (error) {
+        console.error("Error signing in anonymously:", error);
+        return;
+      }
+    }
+
+    const copyData = {
+      gender: formData.gender,
+      age: formData.age,
+      unit: formData.unit,
+      height: formData.height,
+      heightFeet: formData.heightFeet,
+      heightInches: formData.heightInches,
+      weight: formData.weight,
+      waist: formData.waist,
+      hip: formData.hip,
+      neck: formData.neck,
+    };
+
+    // Update the user data in the users table
+    const { error } = await supabase.from("users").upsert(
+      {
+        ...copyData,
+      },
+      {
+        onConflict: "id",
+      },
+    );
+
+    if (error) {
+      console.error("Error updating user data:", error);
+    }
+
+    set({ currentStep: newStep });
+  },
+  setCurrentStep: async (step: number) => {
+    set({ currentStep: step });
+    const { formData } = get();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    // If no user, sign in anonymously
+    if (!user) {
+      const { error } = await supabase.auth.signInAnonymously();
+      if (error) {
+        console.error("Error signing in anonymously:", error);
+        return;
+      }
+    }
+    const copyData = {
+      gender: formData.gender,
+      age: formData.age,
+      unit: formData.unit,
+      height: formData.height,
+      heightFeet: formData.heightFeet,
+      heightInches: formData.heightInches,
+      weight: formData.weight,
+      waist: formData.waist,
+      hip: formData.hip,
+      neck: formData.neck,
+    };
+    // Update the user data in the users table
+    const { error } = await supabase.from("users").upsert({
+      ...copyData,
+    });
+
+    if (error) {
+      console.error("Error updating user data:", error);
+    }
+
+    // Update the state
+  },
+  updateFormData: (field, value) => {
     set((state) => ({
       formData: { ...state.formData, [field]: value },
-    })),
+    }));
+  },
 }));
