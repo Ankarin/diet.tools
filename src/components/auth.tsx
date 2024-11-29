fc479c44cf30015443da8dc2099a8ea5e124649c
mitry <ankarn41k@gmail.com>
2024-11-29 17:12:13 +0200

    ready ?

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 async function Auth() {
   const supabase = await createClient();
-  const { data, error } = await supabase.auth.getSession();
+  const { data } = await supabase.auth.getSession();
   return (
     <div>
       {data.session ? (
