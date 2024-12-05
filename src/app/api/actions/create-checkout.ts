3da30b61d2d525f9a19bcb94d26d68087b32eb4f
mitry <ankarn41k@gmail.com>
2024-12-05 16:53:38 +0200

    fix

--- src/app/api/actions/create-checkout.ts
+++ src/app/api/actions/create-checkout.ts
@@ -7,34 +7,35 @@ import { z } from "zod";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 const checkoutSchema = z.object({
-  priceId: z.string(),
+	priceId: z.string(),
 });
 
 export const createCheckoutSession = actionClient
-  .schema(checkoutSchema)
-  .action(async ({ parsedInput, ctx }): Promise<{ sessionId: string }> => {
-    try {
-      const { priceId } = parsedInput;
+	.schema(checkoutSchema)
+	.action(async ({ parsedInput, ctx }): Promise<{ sessionId: string }> => {
+		try {
+			const { priceId } = parsedInput;
 
-      const user = ctx.user;
+			const user = ctx.user;
 
-      const session = await stripe.checkout.sessions.create({
-        customer_email: user.email,
-        payment_method_types: ["card"],
-        line_items: [
-          {
-            price: priceId,
-            quantity: 1,
-          },
-        ],
-        mode: "subscription",
-        success_url: `${process.env.NEXT_PUBLIC_URL}/me`,
-        cancel_url: `${process.env.NEXT_PUBLIC_URL}/me`,
-      });
+			const session = await stripe.checkout.sessions.create({
+				customer_email: user.email,
+				payment_method_types: ["card"],
+				line_items: [
+					{
+						price: priceId,
+						quantity: 1,
+					},
+				],
+				mode: "subscription",
+				success_url: `${process.env.NEXT_PUBLIC_URL}/me`,
+				cancel_url: `${process.env.NEXT_PUBLIC_URL}/me`,
+				allow_promotion_codes: true,
+			});
 
-      return { sessionId: session.id };
-    } catch (error) {
-      console.error("Error creating checkout session:", error);
-      throw error;
-    }
-  });
+			return { sessionId: session.id };
+		} catch (error) {
+			console.error("Error creating checkout session:", error);
+			throw error;
+		}
+	});
