a5f636b17c3b5aeb5da8bb4aafb6b120fe3cb7cc
mitry <ankarn41k@gmail.com>
2024-12-05 16:15:59 +0200

    fix

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,32 +4,32 @@ import { Redis } from "@upstash/redis";
 import { createSafeActionClient } from "next-safe-action";
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+	url: process.env.UPSTASH_REDIS_REST_URL!,
+	token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 });
 
 const rateLimiter = new Ratelimit({
-  redis,
-  limiter: Ratelimit.fixedWindow(30, "1m"),
+	redis,
+	limiter: Ratelimit.fixedWindow(30, "1m"),
 });
 
 export const actionClient = createSafeActionClient()
-  .use(async ({ next }) => {
-    const supabase = await createClient();
-    const { data, error } = await supabase.auth.getSession();
-    console.log("user", data.session.user);
-    if (error) {
-      throw new Error("Unauthorized");
-    }
-    const user = data.session?.user;
-    return next({ ctx: { user } });
-  })
-  .use(async ({ next, ctx }) => {
-    const { user } = ctx;
-    const { success } = await rateLimiter.limit(user.id);
-    if (!success) {
-      console.log("Rate limit exceeded");
-      throw new Error("Rate limit exceeded");
-    }
-    return next();
-  });
+	.use(async ({ next }) => {
+		const supabase = await createClient();
+		const { data, error } = await supabase.auth.getUser();
+		console.log("user", data.user);
+		if (error) {
+			throw new Error("Unauthorized");
+		}
+		const user = data.user;
+		return next({ ctx: { user } });
+	})
+	.use(async ({ next, ctx }) => {
+		const { user } = ctx;
+		const { success } = await rateLimiter.limit(user.id);
+		if (!success) {
+			console.log("Rate limit exceeded");
+			throw new Error("Rate limit exceeded");
+		}
+		return next();
+	});
